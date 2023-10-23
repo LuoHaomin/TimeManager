c6d64f6b7fa53bd60bdc767de2a9678a19422226
mogoo7zn <znwangking@hotmail.com>
2023-10-23 20:46:06 +0800

    commit by wang

---
+++ app/src/main/java/com/example/timemanager/adapter/WeeklyAdapter.java
@@ -0,0 +1,25 @@
+package com.example.timemanager.adapter;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class WeeklyAdapter extends RecyclerView.Adapter {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
